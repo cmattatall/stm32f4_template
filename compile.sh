e45a9c506831e05b746675d111addc8ea2acaf19
Carl Mattatall <cmattatall2@gmail.com>
2021-02-01 12:13:57 -0400

    add very basic compilation bash script

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#brief example bash script so I don't have to manually type in the cmake generation + build commands
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_CROSSCOMPILING=ON && cmake --build build
